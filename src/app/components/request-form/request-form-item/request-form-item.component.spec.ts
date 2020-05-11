061f998cd9630a281d6c2ccac704ed69886107db
Bryce Meyer <bryce13950@gmail.com>
2020-05-12 01:33:42 +0200

    fixed request form item component

--- src/app/components/request-form/request-form-item/request-form-item.component.spec.ts
+++ src/app/components/request-form/request-form-item/request-form-item.component.spec.ts
@@ -3,12 +3,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RequestFormItemComponent } from './request-form-item.component';
 import { CommonModule } from "@angular/common";
 import { AlertController, IonicModule, NavController } from "@ionic/angular";
+import {Camera} from '@ionic-native/camera/ngx';
+import {RequestsProvider} from '../../../providers/requests/requests';
+import RequestsProviderMock from '../../../providers/requests/requests.mock';
 
 describe('RequestFormItemComponent', () => {
     let component: RequestFormItemComponent;
     let fixture: ComponentFixture<RequestFormItemComponent>;
     let navController;
     let alertController;
+    const requestsProvider: RequestsProvider = new RequestsProviderMock();
+    const camera: Camera = new Camera();
 
     beforeEach(async(() => {
         navController = jasmine.createSpyObj('NavController', ['goBack']);
@@ -21,6 +26,8 @@ describe('RequestFormItemComponent', () => {
             providers: [
                 {provide: AlertController, useValue: alertController},
                 {provide: NavController, useValue: navController},
+                { provide: RequestsProvider, useValue: requestsProvider},
+                {provide: Camera, useValue: camera},
             ],
             declarations: [
                 RequestFormItemComponent,
